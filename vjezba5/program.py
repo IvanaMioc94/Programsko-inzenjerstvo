365a5963f451210321f74f4b1186dc31bd7ff508
IvanaMioc94 <57687470+IvanaMioc94@users.noreply.github.com>
2020-04-30 22:54:09 +0200

    Add files via upload

---
+++ vjezba5/program.py
@@ -0,0 +1,9 @@
+from likovi import *
+from funkcije import *
+
+
+
+k1 = Kruznica(3)
+k2 = Kvadrat(5)
+print(k1,'opsega', opseg(k1),'povrsine', povrsina(k1))
+print(k2,'opsega', opseg(k2),'povrsine', povrsina(k2))
